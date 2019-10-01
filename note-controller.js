bf9fbd6d9507dcebbf04dcdb0b865a3228708686
Ben <ben@benzeng.co.uk>
2019-10-01 16:48:54 +0100

    spiked showing note in index.html (ex 6)

--- note-controller.js
+++ note-controller.js
@@ -1,1 +1,17 @@
-document.getElementById("app").innerHTML = "howdy";
+
+
+function noteController(noteListViewArg, noteListArg, text = "Favourite drink: seltzer ") {
+    let noteControllerText = text;
+
+    let noteList = new NoteList();
+    noteList.add(noteControllerText);
+
+    let noteListView = new NoteListView(noteList);
+
+
+    document.getElementById("app").innerHTML = noteListView.returnNotesHTML();
+
+}
+console.log("noteController being called");
+
+ noteController(NoteListView,NoteList);
